c393f8e58a68f54db1ee2f5a96c9a2b377faca52
sooshe <sooshe@sooshebot.com>
2020-02-21 12:15:52 -0600

    create getprojectTasks and addProject functions

--- helpers/projectModel.js
+++ helpers/projectModel.js
@@ -2,30 +2,48 @@ const db = require("../data/dbConfig");
 
 module.exports = {
   getProject,
-  getProjectId
-  // getProjectTask,
-  // addProject,
-  // updateProject,
-  // removeProject
+  getProjectId,
+  getProjectTask,
+  addProject,
+  updateProject,
+  removeProject
 };
 
 function getProject() {
   return db("projects");
-};
+}
 
 function getProjectId(project_id) {
   return db("projects as p")
     .select(
-        "p.id as id",
-        "p.project_name as name",
-        "p.description as description",
-        "p.completed as completed",
-        "t.id as task id",
-        "t.description as description",
-        "t.notes as notes",
-        "t.completed as completed"
+      "p.id as id",
+      "p.project_name as name",
+      "p.description as description",
+      "p.completed as completed",
+      "t.id as task id",
+      "t.description as description",
+      "t.notes as notes",
+      "t.completed as completed"
     )
     .innerJoin("tasks as t", "t.project_id", "=", "p.id")
     .where("project_id", project_id)
     .first();
-};
+}
+
+function getProjectTask(project_id) {
+  return db("projects as p")
+    .select(
+      "p.id as id",
+      "p.project_name as name",
+      "p.completed as completed",
+      "t.id as task id",
+      "t.description as task description",
+      "t.completed as completed"
+    )
+    .join("tasks as t", "t.project_id", "=", "p.id")
+    .where("project_id", project_id);
+}
+
+function addProject(project) {
+  return db("projects").insert(project);
+}
