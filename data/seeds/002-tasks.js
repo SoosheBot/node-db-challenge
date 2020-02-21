2b5604a600c019f2732467f0ce99e5b56c05d591
sooshe <sooshe@sooshebot.com>
2020-02-21 11:07:44 -0600

    complete tasks seed

--- data/seeds/002-tasks.js
+++ data/seeds/002-tasks.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('tasks').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('tasks').insert([
+        {id: 1, description: 'Mow lawn', notes: "Task 1", completed: false},
+        {id: 2, description: 'Measure lawn', notes: "Task 2", completed: false},
+        {id: 3, description: 'Fertilize lawn', notes: "Task 3", completed: false},
+        {id: 4, description: 'Water lawn', notes: "Task 4", completed: false},
+        {id: 5, description: 'Weed lawn', notes: "Task 5", completed: false},
+        {id: 6, description: 'Admire lawn', notes: "Task 6", completed: false}
       ]);
     });
 };
