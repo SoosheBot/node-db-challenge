6d7db1e702014a1d3f0006bb3a6b8bcbb4bb7159
sooshe <sooshe@sooshebot.com>
2020-02-21 12:11:53 -0600

    quick stretch attempt, back to mvp work

--- helpers/projectModel.js
+++ helpers/projectModel.js
@@ -13,8 +13,19 @@ function getProject() {
   return db("projects");
 };
 
-function getProjectId(id) {
-  return db("recipes")
-    .where({ id })
+function getProjectId(project_id) {
+  return db("projects as p")
+    .select(
+        "p.id as id",
+        "p.project_name as name",
+        "p.description as description",
+        "p.completed as completed",
+        "t.id as task id",
+        "t.description as description",
+        "t.notes as notes",
+        "t.completed as completed"
+    )
+    .innerJoin("tasks as t", "t.project_id", "=", "p.id")
+    .where("project_id", project_id)
     .first();
 };
--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -4,18 +4,53 @@ const Project = require("../helpers/projectModel");
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    Project.getProject()
+router.get("/", (req, res) => {
+  Project.getProject()
     .then(projects => {
-        if (projects) {
-            res.status(200).json(projects);
-        } else {
-            res.status(400).json({ errorMessage: "Projects may not exist."})
-        };
+      if (projects) {
+        res.status(200).json(projects);
+      } else {
+        res.status(400).json({ errorMessage: "Projects may not exist." });
+      }
     })
     .catch(err => {
-        res.status(500).json({ errorMessage: "Could not get projects"})
+      res.status(500).json({ errorMessage: "Could not get projects" });
     });
 });
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Project.getProjectId(id)
+    .then(project => {
+        if (project) {
+            res.status(200).json(project);
+        } else {
+            res.status(400).json({ errorMessage: "Project/ID does not exist."})
+        } 
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ errorMessage: "Could not find project with this ID." });
+    });
+});
+
+//CUSTOM MIDDLEWARE
+function validateProjectId(req, res, next) {
+  Project.get(req.params.id)
+    .then(checkId => {
+      if (checkId) {
+        req.checkId = checkId;
+        next();
+      } else {
+        res.status(404).json({ errorMessage: "Project ID may not exist" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Could not verify ID." });
+    });
+};
 
 module.exports = router;
