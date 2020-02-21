ceab57942913c976551e1cc505193c310da4ba77
sooshe <sooshe@sooshebot.com>
2020-02-21 10:43:34 -0600

    complete projects table in migrations

--- data/migrations/20200221102740_projects.js
+++ data/migrations/20200221102740_projects.js
@@ -1,8 +1,33 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("tasks", tbl => {
+      tbl.increments();
+      tbl.text("description", 280).notNullable();
+      tbl.text("notes", 280);
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+    .createTable("resources", tbl => {})
+    .createTable("projects", tbl => {
+      tbl.increments();
+      tbl.text("project_name", 255).notNullable();
+      tbl.text("description");
+      tbl
+        .boolean("completed")
+        .defaultTo(false)
+        .notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("projects")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("tasks");
 };
