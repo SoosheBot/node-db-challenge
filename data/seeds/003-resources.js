277641df6aafbfee0ce44e8513afff2db6862492
sooshe <sooshe@sooshebot.com>
2020-02-21 11:10:52 -0600

    complete resources seed

--- data/seeds/003-resources.js
+++ data/seeds/003-resources.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('resources').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('resources').insert([
+        {id: 1, name: 'lawnmower', description: "heavy and teethy"},
+        {id: 2, name: 'fertilizer', description: "heavy and stinky"},
+        {id: 3, name: 'measuring tape', description: "somewhat accurate"},
+        {id: 4, name: 'leaf blower', description: "heavy and noisy"},
+        {id: 5, name: 'water hose', description: "heavy and damp"},
+        {id: 6, name: 'gardening gloves', description: "protective and cozy"},
       ]);
     });
 };
