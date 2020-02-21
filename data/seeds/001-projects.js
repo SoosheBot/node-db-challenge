e6b7913884ce466ebee7300e167ae3d73ba98cf2
sooshe <sooshe@sooshebot.com>
2020-02-21 11:04:57 -0600

    complete projects seed

--- data/seeds/001-projects.js
+++ data/seeds/001-projects.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        {id: 1, project_name: 'project 1', description: '1st project', completed: false},
+        {id: 2, project_name: 'project 2', description: '2nd project', completed: false},
+        {id: 3, project_name: 'project 3', description: '3rd project', completed: false},
+        {id: 4, project_name: 'project 4', description: '4th project', completed: false},
+        {id: 5, project_name: 'project 5', description: '5th project', completed: false},
+        {id: 6, project_name: 'project 6', description: '6th project', completed: false},
       ]);
     });
 };
