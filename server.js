c9571a164fb722d9567bc8c072343babe02bfd8e
sooshe <sooshe@sooshebot.com>
2020-02-21 11:30:49 -0600

    complete server.js file, create project model and router files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+const projectRouter = require("./routers/projectRouter");
+
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/projects', projectRouter);
+
+module.exports = server;
